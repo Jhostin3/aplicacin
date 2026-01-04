70c7e65853caab95928eadf5f5e07c4a38c4767b
jhostinjosue23 <jhostinjosue23@gmail.com>
2026-01-04 19:28:39 +0000

    ✨feat: estructura base de profile card con NativeWind

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native';
+import { View, Text } from "react-native";
 
 export default function ProfileScreen() {
   return (
     <View className="flex-1 justify-center items-center bg-gray-100">
-      <Text className="text-lg">Profile Screen</Text>
+      <View className="w-11/12 bg-white rounded-2xl p-6 shadow-lg">
+        <Text className="text-2xl font-bold text-center">Profile</Text>
+      </View>
     </View>
   );
 }
