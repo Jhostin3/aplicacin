0401142b1511d9ea26c042a0d79ecfd37e4da776
jhostinjosue23 <jhostinjosue23@gmail.com>
2026-01-04 19:29:44 +0000

    ✨ feat: imagen superior del perfil con NativeWind

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 export default function ProfileScreen() {
   return (
     <View className="flex-1 justify-center items-center bg-gray-100">
-      <View className="w-11/12 bg-white rounded-2xl p-6 shadow-lg">
-        <Text className="text-2xl font-bold text-center">Profile</Text>
+      <View className="w-11/12 bg-white rounded-2xl shadow-lg">
+        <Image
+          className="w-full h-48 rounded-t-2xl"
+          source={{ uri: "https://picsum.photos/seed/696/3000/2000" }}
+          resizeMode="cover"
+        />
+        <View className="p-6">
+          <Text className="text-2xl font-bold text-center">Profile</Text>
+        </View>
       </View>
     </View>
   );
